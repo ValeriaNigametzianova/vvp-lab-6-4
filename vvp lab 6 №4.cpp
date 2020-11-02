2250737aca4ac1dfc3bef34b36360bc7d3be377c
ValeriaNigametzianova <71436617+ValeriaNigametzianova@users.noreply.github.com>
2020-11-02 14:19:16 +0300

    Update vvp lab 6 №4.cpp

--- vvp lab 6 №4.cpp
+++ vvp lab 6 №4.cpp
@@ -1,12 +1,12 @@
-﻿#include<stdio.h>
+#include<stdio.h>
 
 int main()
 {
 	int y, x, t;
 	printf("x = ");
 	scanf_s("%d", &x);
-	t = x * x;
-	y = 3 * t * t * t - 6 * t - 7;
+	t = x * x; // заменим x^2 на переменную 
+	y = 3 * t * t * t - 6 * t - 7; // теперь строка с вычислением значения функции выглядит проще
 	printf("y = %d", y);
 	return 0;
 }
